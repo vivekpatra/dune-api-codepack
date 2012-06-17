ac6483aae9f5fd75312b45562c5fdeb873bb4034
Steven Liekens <steven.liekens@gmail.com>
2012-06-17 21:18:28 +0200

    06/17/2012
    Bumped the version number to 0.9.10
    This is the first release for protocol version 3 that is stable enough to run, graciously handle disconnects, etc.
    To my knowledge it is fully backwards compatible with version 1 and 2.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.9.0")> 
-<Assembly: AssemblyFileVersion("0.9.9.0")> 
+<Assembly: AssemblyVersion("0.9.10.0")> 
+<Assembly: AssemblyFileVersion("0.9.10.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
 <Assembly: CLSCompliant(True)> 
