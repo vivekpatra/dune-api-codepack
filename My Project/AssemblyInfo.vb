a9be981b90ac89f77266092b05fc0abb6a38fc22
Steven Liekens <steven.liekens@gmail.com>
2012-06-17 22:28:41 +0200

    06/17/2012
    First official release of many to come!
    Switched to a more meaningful software versioning strategy.
    Added a copy of the GPL and LGPL and license notices in source files.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -1,4 +1,23 @@
-﻿Imports System.Resources
+﻿#Region "License"
+' Copyright 2012 Steven Liekens
+' Contact: steven.liekens@gmail.com
+
+' This file is part of DuneApiCodepack.
+
+' DuneApiCodepack is free software: you can redistribute it and/or modify
+' it under the terms of the GNU Lesser General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+
+' DuneApiCodepack is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU Lesser General Public License for more details.
+
+' You should have received a copy of the GNU Lesser General Public License
+' along with DuneApiCodepack.  If not, see <http://www.gnu.org/licenses/>.
+#End Region ' License
+Imports System.Resources
 
 Imports System
 Imports System.Reflection
@@ -33,8 +52,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.10.0")> 
-<Assembly: AssemblyFileVersion("0.9.10.0")> 
+<Assembly: AssemblyVersion("3.0.0.0")> 
+<Assembly: AssemblyFileVersion("3.2012.0617.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
 <Assembly: CLSCompliant(True)> 
--- Networking/NetworkAdapterInfo.vb
+++ Networking/NetworkAdapterInfo.vb
@@ -1,4 +1,23 @@
-﻿Imports System.Net
+﻿#Region "License"
+' Copyright 2012 Steven Liekens
+' Contact: steven.liekens@gmail.com
+
+' This file is part of DuneApiCodepack.
+
+' DuneApiCodepack is free software: you can redistribute it and/or modify
+' it under the terms of the GNU Lesser General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+
+' DuneApiCodepack is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU Lesser General Public License for more details.
+
+' You should have received a copy of the GNU Lesser General Public License
+' along with DuneApiCodepack.  If not, see <http://www.gnu.org/licenses/>.
+#End Region ' License
+Imports System.Net
 Imports System.Net.Sockets
 Imports System.Net.NetworkInformation
 Imports System.Threading.Tasks
